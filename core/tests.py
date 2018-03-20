26aa6112a604cf0e5e1241c358edd136cc3792ba
Luciano Ratamero <luciano@ratamero.com>
2018-03-20 03:29:27 -0300

    refactors views ans tests to make use of the django_apistar app.

--- core/tests.py
+++ core/tests.py
@@ -1,68 +1,43 @@
-from app import app
 
-from unittest.mock import patch
+from django.test import TestCase
 from apistar.test import TestClient
-from apistar.backends.django_orm import migrate, flush
+from model_mommy import mommy
+from django_apistar.apps import App
 
-from core import views, models
-
-# we need to manually create the database, since we're using py.test directly
-migrate()
+from core import models, schemas
 
 
-def test_welcome():
-    """
-    Testing a view directly.
-    """
-    data = views.welcome()
-    assert data == {'message': 'Welcome to API Star!'}
+class TestCreateProduct(TestCase):
+
+    def test_criar_produto_unmocked(self):
+        """
+        Testing a view that uses django_orm.Session, no mocks, using the test client.
+        """
+        client = TestClient(App)
+        post_data = {'nota': 5, 'nome': 'chocolate', 'tamanho': 'grande'}
+        assert models.Produto.objects.count() == 0
+
+        response = client.post('/criar-produto/', data=post_data)
+
+        assert response.status_code == 201
+        assert response.json() == post_data
+
+        assert models.Produto.objects.count() == 1
+        db_produto = models.Produto.objects.get()
+        assert db_produto.nome == post_data['nome']
+        assert db_produto.nota == post_data['nota']
+        assert db_produto.tamanho == post_data['tamanho']
 
 
-def test_http_request():
-    """
-    Testing a view, using the test client.
-    """
-    client = TestClient(app)
-    response = client.get('http://localhost/')
-    assert response.status_code == 200
-    assert response.json() == {'message': 'Welcome to API Star!'}
+class TestListProducts(TestCase):
 
+    def test_listar_produtos(self):
+        client = TestClient(App)
+        produto = mommy.make(models.Produto, nota=5, tamanho='grande')
 
-@patch('apistar.backends.django_orm.Session')
-def test_criar_produto(mocked_session):
-    """
-    Testing a view that uses django_orm.Session, mocking, using the test client.
-    """
-    post_data = {'nota': 5, 'nome': 'chocolate', 'tamanho': 'grande'}
-    mocked_session().Produto().nome = post_data['nome']
-    mocked_session().Produto().nota = post_data['nota']
-    mocked_session().Produto().tamanho = post_data['tamanho']
+        response = client.get('/listar-produtos/')
+        content = response.json()
 
-    client = TestClient(app)
-    response = client.post('/criar-produto', data=post_data)
-
-    mocked_session().Produto.assert_called_with(**post_data)
-    mocked_session().Produto().save.assert_called_once_with()
-    assert response.status_code == 201
-    assert response.json() == post_data
-
-
-def test_criar_produto_unmocked():
-    """
-    Testing a view that uses django_orm.Session, no mocks, using the test client.
-    """
-    post_data = {'nota': 5, 'nome': 'chocolate', 'tamanho': 'grande'}
-    assert models.Produto.objects.count() == 0
-
-    client = TestClient(app)
-    response = client.post('/criar-produto', data=post_data)
-
-    assert response.status_code == 201
-    assert response.json() == post_data
-
-    assert models.Produto.objects.count() == 1
-    db_produto = models.Produto.objects.get()
-    assert db_produto.nome == post_data['nome']
-    assert db_produto.nota == post_data['nota']
-    assert db_produto.tamanho == post_data['tamanho']
-    flush()  # remember to clean the db after using it
+        expected_produto = schemas.Produto(produto.__dict__)
+        self.assertEqual(1, len(content))
+        self.assertEqual(expected_produto, content[0])
--- core/views.py
+++ core/views.py
@@ -2,7 +2,6 @@
 from apistar import http, annotate
 from apistar.interfaces import Auth
 from apistar.permissions import IsAuthenticated
-from apistar.backends.django_orm import Session
 
 from core import schemas
 
@@ -20,17 +19,19 @@ def me(auth: Auth):
     }
 
 
-def criar_produto(produto: schemas.Produto, session: Session):
+def criar_produto(produto: schemas.Produto):
+    from core.models import Produto
     if not produto:
         return
 
-    db_produto = session.Produto(**produto)
+    db_produto = Produto(**produto)
     db_produto.save()
     return http.Response(content=schemas.Produto(db_produto.__dict__), status=201)
 
 
-def listar_produtos(nota: schemas.Nota, session: Session):
-    produtos = session.Produto.objects.all()
+def listar_produtos(nota: schemas.Nota):
+    from core.models import Produto
+    produtos = Produto.objects.all()
     if nota is not None:
         produtos = produtos.filter(nota=nota)
     return [schemas.Produto(db_produto.__dict__) for db_produto in produtos]
@@ -40,16 +41,18 @@ def valida_cpf(cpf: schemas.CPF):
     return cpf
 
 
-def criar_usuario(user: schemas.User, session: Session):
+def criar_usuario(user: schemas.User):
+    from django.contrib.auth.models import User
     if not user:
         return
-    db_user = session.User.objects.create_user(**user)
+    db_user = User.objects.create_user(**user)
     return schemas.User(db_user.__dict__)
 
 
 @annotate(permissions=[IsAuthenticated()])
-def listar_usuarios(username: schemas.Username, session: Session):
-    usuarios = session.User.objects.all()
+def listar_usuarios(username: schemas.Username):
+    from django.contrib.auth.models import User
+    usuarios = User.objects.all()
     if username is not None:
         usuarios = usuarios.filter(username=username)
     return [schemas.User(db_user.__dict__) for db_user in usuarios]
